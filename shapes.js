ff40c658ccdbdd484f531796021b6b044b96e9fc
Marie Antons <chefantons@gmail.com>
2020-06-27 17:44:38 -0500

    rectangle data layout

--- shapes.js
+++ shapes.js
@@ -1,5 +1,16 @@
 var dataArray = [5,11,18];
 
-d3.select("body").append("svg")
+var svg = d3.select("body").append("svg")
     .attr("height","100%")
     .attr("width","100%");
+
+    svg.selectAll('rect')
+    .data(dataArray)
+    .enter().append('rect')
+    .attr("height",function(d,i){return d*15;})
+    .attr("width","50")
+    //to make dynamic we need to use a function d = data point i = index
+    .attr("x",function(d,i){return 60*i;})
+    //to invert we need to create a function and subtract the height
+    .attr("y",function(d,i){return 300-(d*15);});
+
