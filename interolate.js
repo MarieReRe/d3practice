b966461bde8b13341216654acc3aaad1638e10c0
Marie Antons <chefantons@gmail.com>
2020-06-28 11:03:10 -0500

    line graph

---
+++ interolate.js
@@ -0,0 +1,18 @@
+var dataArray = [{ x: 2, y: 15 }, { x:8, y: 20 }, { x: 16, y:29 }, { x: 20, y: 65 }, { x: 32, y: 50 }];
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("height", "100%")
+    .attr("width", "100%");
+
+
+//d is data 
+var line = d3.line()
+    .x(function(d){return d.x*6;})
+    .y(function(d){return d.y*4;})
+
+
+svg.append("path")
+.attr("fill", "none")
+.attr("stroke", "blue")
+.attr("d", line(dataArray));
